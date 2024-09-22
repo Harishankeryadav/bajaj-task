2e50cb9093e7c0ac77273b8f1d5795f3f5212c43
Hari shanker yadav <113604005+Harishankeryadav@users.noreply.github.com>
2024-09-22 19:04:28 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,35 @@
 
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const bfhlRoutes = require('./routes/bfhl'); 
+
+// const app = express();
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use('/bfhl', bfhlRoutes); 
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const bfhlRoutes = require('./routes/bfhl'); 
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors({
+  origin: ["https://bajaj-task-frontend-nu.vercel.app"],
+  methods: ["POST", "GET"],
+  credentials: true
+}));
 
 app.use('/bfhl', bfhlRoutes); 
 
