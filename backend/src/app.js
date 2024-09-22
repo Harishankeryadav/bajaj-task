407ede034eda21781eab4a9099cd26f7480f5790
Hari shanker yadav <113604005+Harishankeryadav@users.noreply.github.com>
2024-09-22 20:09:39 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -34,6 +34,10 @@ app.use(cors({
 app.use('/bfhl', bfhlRoutes); 
 
 const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+  res.send('Welcome to the BFHL API!');
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
